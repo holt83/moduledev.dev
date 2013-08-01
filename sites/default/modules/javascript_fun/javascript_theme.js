9be325ffea23998e189863868cfd4f845af613d7
Simon Holt <simonholt83@gmail.com>
2013-08-01 07:57:22 -0500

    Done for today

--- sites/default/modules/javascript_fun/javascript_theme.js
+++ sites/default/modules/javascript_fun/javascript_theme.js
@@ -1,3 +1,4 @@
+
 (function($) {
   // Declare a theme function
   Drupal.theme.prototype.hello = function(text) {
@@ -5,10 +6,23 @@
   }
   // Attach a behavior:
   Drupal.behaviors.helloWorld = {
+    
     attach: function(context, settings) {
-      alert(Drupal.theme('hello', 'Randers') + ' ' + context + ' ' + settings);
+      
+      $('#ajax_markup_div', context).once('ajax_markup_div', function() {
+        alert(Drupal.theme('hello', 'Randers') + ' ' + context + ' ' + settings);
+        alert(Drupal.settings.helloWorld.message);
+        alert('Attach function for helloWorld behavior is done!');
+        alert('This alert only runs ones per AJAX request');
+      }); 
     }
+    
   };
+  
+  $(document).ready(function() {
+    alert('This alert only runs ones per full page request');
+  });
+  
 })(jQuery);
 
 
