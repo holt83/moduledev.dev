2019b3382203a5c44ba1c56213e9dfef9f5945de
Simon Holt <simonholt83@gmail.com>
2013-08-02 07:51:40 -0500

    Done for today

--- sites/default/modules/javascript_fun/javascript_theme.js
+++ sites/default/modules/javascript_fun/javascript_theme.js
@@ -1,19 +1,27 @@
 
-(function($) {
+/*(function($) {
   // Declare a theme function
   Drupal.theme.prototype.hello = function(text) {
     return Drupal.t('Hello @city', {'@city' : text}); 
   }
-  // Attach a behavior:
+  // Define a behavior for our module:
   Drupal.behaviors.helloWorld = {
     
+    // Attach function: Runs when new content is added to the DOM.
     attach: function(context, settings) {
-      
-      $('#ajax_markup_div', context).once('ajax_markup_div', function() {
+      $('#ajax_markup_div', context).once('ajax_markup_div_attach', function() {
         alert(Drupal.theme('hello', 'Randers') + ' ' + context + ' ' + settings);
         alert(Drupal.settings.helloWorld.message);
-        alert('Attach function for helloWorld behavior is done!');
         alert('This alert only runs ones per AJAX request');
+      });
+    },
+    
+    
+    // Detach function: Runs when content is removed from the DOM.
+    detach: function (context, settings, trigger) {      
+      $('#ajax_markup_div', context).once('ajax_markup_div_detach', function() {
+        alert('This is a detach function');
+        console.log(trigger);
       }); 
     }
     
@@ -23,6 +31,6 @@
     alert('This alert only runs ones per full page request');
   });
   
-})(jQuery);
+})(jQuery);*/
 
 
